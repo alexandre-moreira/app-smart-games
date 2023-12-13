328b6241f165b48f360cd9e1be281333addc7ef7
Alexandre Moreira <4lexandre.moreira@gmail.com>
2023-12-13 15:52:17 -0300

    fix: update routes

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,6 +1,5 @@
 import {Router} from 'express';
 
-//import { getUsers, createUser, deleteUser } from './controllers/UserController.js';
 import { getProducts, createProduct, deleteProduct } from './controllers/ProductController.js';
 
 const routes = Router();
@@ -8,9 +7,5 @@ const routes = Router();
 routes.get('/products', getProducts);
 routes.post('/products', createProduct);
 routes.delete('/products/:id', deleteProduct);
-
-// routes.get('/users', getUsers);
-// routes.post('/users', createUser);
-// routes.delete('/users/:id', deleteUser);
 
 export default routes;
